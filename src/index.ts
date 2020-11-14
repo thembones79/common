e6348d7d4ff7b14b4b1da432cbbaff63fef94463
thembones79 <michal.szumnarski@gmail.com>
2020-11-14 21:38:15 +0100

    patch

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 10,
+  green: 20,
+  blue: 5,
+};
+
+console.log(color);
+
+export default Color;
